1f0f0117fdf55848b01f8d75f5fed3a13ffc5546
ExtremisMalis <aep.aen.seidhe@gmail.com>
2024-11-09 15:57:01 +0500

    Update participantList.js

--- participantList.js
+++ participantList.js
@@ -1,13 +1,16 @@
 async function fetchParticipants() {
-  const response = await fetch('https://raw.githubusercontent.com/ExtremisMalis/participant-list-demo/refs/heads/main/participants.json');
+  const response = await fetch('https://raw.githubusercontent.com/ExtremisMalis/participant-list-demo/main/participants.json');
   const participants = await response.json();
   
-  const participantList = document.querySelectorAll('participant-list');
-  participantList.innerHTML = '';
-  participants.forEach(participant => {
-    const listItem = document.createElement('p');
-    listItem.textContent = participant.username;
-    participantList.appendChild(listItem);
+  const participantLists = document.querySelectorAll('.participant-list'); 
+  
+  participantLists.forEach(participantList => {
+    participantList.innerHTML = ''; 
+    participants.forEach(participant => {
+      const listItem = document.createElement('p');
+      listItem.textContent = participant.username;
+      participantList.appendChild(listItem);
+    });
   });
 }
 
