9764d3eb15efea1619b7f3a7992bf4375e939080
ExtremisMalis <aep.aen.seidhe@gmail.com>
2024-11-09 15:28:17 +0500

    Update participantList.js

--- participantList.js
+++ participantList.js
@@ -7,7 +7,7 @@ const Users = [
 function addUserToList(userId) {
   const user = Users.find(u => u.id === userId);
   if (user) {
-    const userList = document.getElementById('userList');
+    const userList = document.getElementByClass('userList');
     if (!userList.innerHTML.includes(user.username)) { 
       userList.innerHTML += `<li>${user.username}</li>`;
     }
